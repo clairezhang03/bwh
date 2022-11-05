d57b7e561ac6da4c26e45767abc57e29c238b51f
kmeichao <kayleemeichao@gmail.com>
2022-11-04 21:46:36 -0700

    Added basic navigation

---
+++ bwh/StackNavigator.js
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Home from "./screens/Home"
+
+const Stack = createNativeStackNavigator();
+
+export default function StackNavigator() {
+    return (
+        <Stack.Navigator screenOptions={{ headerShown: true }}>
+            <Stack.Screen name="Home" component={Home} />
+
+        </Stack.Navigator>
+
+    )
+}
+
+const styles = StyleSheet.create({})
+
+
---
+++ bwh/screens/Home.js
@@ -0,0 +1,14 @@
+import { SafeAreaView, StyleSheet, Text, View, ScrollView } from 'react-native'
+import React from 'react'
+
+export default function Home() {
+    return (
+        <SafeAreaView>
+            <ScrollView>
+                <Text>bwh.</Text>
+            </ScrollView>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({})
