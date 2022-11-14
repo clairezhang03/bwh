45d08ad6b57bc887107c27d31bd43e8c5df47d21
jebbica <jessicalin4903@gmail.com>
2022-11-13 16:30:20 -0800

    added icons and styling to tab navigation

--- bwh/TabNavigator.js
+++ bwh/TabNavigator.js
@@ -3,12 +3,38 @@ import React from 'react';
 import Home from './screens/Home';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import Placeholder from './screens/Placeholder';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 
 const Tab = createBottomTabNavigator();
 
 export default function TabNavigator() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+
+        if (route.name === 'Home') {
+          iconName = focused ? 'home' : 'home-outline';
+        }
+        else if (route.name === 'Search') {
+          iconName = focused ? 'search' : 'search-outline';
+        }
+        else if (route.name === "Leaderboard") {
+          iconName = focused ? 'trophy' : 'trophy-outline';
+
+        }
+        return <Ionicons name={iconName} size={size} color={color} />;
+      },
+      tabBarStyle:{
+        borderTopWidth: 1,
+        borderColor: "white",
+      },
+      
+      tabBarActiveTintColor: '#6886d9',
+      tabBarInactiveTintColor: 'gray',
+    })}>
         <Tab.Screen name = "Home" component={Home} options={{headerShown: false}}/>
         <Tab.Screen name = "Search" component={Placeholder} options={{headerShown: false}}/>
         <Tab.Screen name = "Leaderboard" component={Placeholder} options={{headerShown: false}}/>
